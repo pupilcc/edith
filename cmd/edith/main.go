00a1dc437267a3cdf87d8bc2aeba733be2a5d285
pupilcc <cc@pupilcc.com>
2023-12-27 21:59:07 +0800

    feat: SetWebhook err info

--- cmd/edith/main.go
+++ cmd/edith/main.go
@@ -5,6 +5,7 @@ import (
 	"edith/internal/middleware"
 	"edith/internal/telegram"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -22,15 +23,19 @@ func main() {
 		})
 	})
 
-	// Telegram Bot
-	b := telegram.GetBot()
-	_ = b.SetWebhook(telegram.GetWebhook())
-
 	// Routes
 	api.WebhookRoutes(e)
 
 	// Logger
 	e.Use(middleware.RequestLogger())
+	logger := middleware.GetLogger()
+
+	// Telegram Bot
+	b := telegram.GetBot()
+	err := b.SetWebhook(telegram.GetWebhook())
+	if err != nil {
+		logger.Error("Error setting webhook: ", zap.Error(err))
+	}
 
 	// Start the service
 	e.Logger.Fatal(e.Start(":1455"))
